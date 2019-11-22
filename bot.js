dcb113484bee306b0488727512d238c5a77ef653
Toxic123xd <58085100+Toxic123xd@users.noreply.github.com>
2019-11-22 20:22:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`عندي سيرفر ريوارد اسطوري تعالو`,"http://twitch.tv/ToxicCity")
 client.user.setStatus("dnd")
 });
 
